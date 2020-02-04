f6e0415f8c0a70d5f649662460ea8398db4b290a
seoje <seoje@DESKTOP-S59A773>
2020-02-04 12:04:51 +0900

    20200204 first commit

---
+++ src/semi/cf/jsp/exception/cfException.java
@@ -0,0 +1,17 @@
+package semi.cf.jsp.exception;
+
+public class cfException extends Exception {
+
+	// 사용자 정의 예외
+	// 	예외가 발생할 경우 개발자나 사용자가 알기 쉬운
+	//  문장을 미리 등록하여 예외 처리를 보다 쉽게 하기 위한 용도로 사용하는 클래스
+	
+	public cfException() {
+		super();
+	}
+
+	public cfException(String msg) {
+		super(msg);
+	}
+	
+}
